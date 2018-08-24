81212f7b5ca41803afc5d16249c334f758a6fdc8
Marten Lohstroh <marten@berkeley.edu>
2018-08-23 17:07:00 -0700

    Added sharpSAT (run ./install_deps)

--- ltl_parser/install_deps.sh
+++ ltl_parser/install_deps.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 pip2 install ply & pip2 install pytest
+mkdir lib
+cd lib
+wget https://github.com/marcthurley/sharpSAT/archive/master.zip
+unzip master.zip
+rm master.zip
+mv sharpSAT-master sharpSAT
+cd sharpSAT
+./setupdev.sh
+cd build/Release
+make
