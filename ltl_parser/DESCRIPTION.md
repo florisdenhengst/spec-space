27fe736c06a2f97a4e2e2cfebc937ef176e83e2b
Antonio Iannopollo <antonio@berkeley.edu>
2018-08-27 16:30:19 -0700

    add setup

---
+++ ltl_parser/DESCRIPTION.md
@@ -0,0 +1,4 @@
+a PYthon library for LTL Parsing
+==========================================
+
+This library provide a minimal set of tools to manipulate LTL formulae.
---
+++ ltl_parser/spec_space/__init__.py
@@ -0,0 +1,30 @@
+'''
+Created on Aug 19, 2014
+
+@author: antonio
+'''
+
+import logging
+import sys
+import os
+import pdb
+
+sys.setrecursionlimit(10000)
+
+LOG = logging.getLogger('spec_space')
+LOG.setLevel(logging.INFO)
+
+ch = logging.StreamHandler()
+#ch.setLevel(logging.DEBUG)
+
+# create formatter and add it to the handlers
+formatter = logging.Formatter('%(asctime)s - %(filename)s:%(lineno)d  - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+
+# add the handlers to the logger
+LOG.addHandler(ch)
+
+LOG.debug('spec_space INIT')
+
+
+#pdb.set_trace()
